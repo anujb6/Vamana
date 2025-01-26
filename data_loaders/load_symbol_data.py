73e4571663a68b6c7b960e42196c67ce40d2830c
Anuj Bhor <bhoranuj6@gmail.com>
2025-01-27 01:36:08 +0530

    changes

--- data_loaders/load_symbol_data.py
+++ data_loaders/load_symbol_data.py
@@ -5,13 +5,12 @@ from tqdm import tqdm
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import meta_data_utils
 
-df = pd.read_csv("data/equity_symbols.csv")
-
+df = pd.read_csv(r"")
 df = df[['SYMBOL', 'NAME OF COMPANY']]
 tqdm.pandas()
 industry_data = df['SYMBOL'].apply(meta_data_utils.fetch_industry_info)
 industry_df = pd.DataFrame(industry_data.tolist())
 df = pd.concat([df, industry_df], axis=1)
-df.to_csv(r'C:\Users\AnujBhor\Desktop\nsepython\data\symbol_data.csv')
+df.to_csv(r'')
 
 
--- utils/meta_data_utils.py
+++ utils/meta_data_utils.py
@@ -1,23 +1,25 @@
 from nsepython import *
 
 def fetch_industry_info(symbol):
-    print(symbol)
+    if '&' in symbol:
+        symbol = symbol.replace('&', '%')
+
     url = f"https://www.nseindia.com/api/quote-equity?symbol={symbol}"
     try:
         payload = nsefetch(url)
         industry_info = payload.get('industryInfo', {})
+        print(symbol)
         return {
             'macro': industry_info.get('macro', None),
             'sector': industry_info.get('sector', None),
             'industry': industry_info.get('industry', None),
             'basicIndustry': industry_info.get('basicIndustry', None)
         }
-        print(symbol)
     except Exception as e:
         print(f"Error fetching data for symbol {symbol}: {e}")
         return {
-            'macro': None,
-            'sector': None,
-            'industry': None,
-            'basicIndustry': None
+            'macro': 'None',
+            'sector': 'None',
+            'industry': 'None',
+            'basicIndustry': 'None'
         }
