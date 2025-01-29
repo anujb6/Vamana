fb304feb191bf391fd10c038d1c88642bd8f8b4e
John Doe <johndoe@example.com>
2025-01-29 13:45:17 +0530

    removed comments

--- visulization/sector_visualization.py
+++ visulization/sector_visualization.py
@@ -5,10 +5,9 @@ import plotly.express as px
 df = pd.read_csv(r'')
 fig = px.sunburst(
     df,
-    path=['macro sector', 'sector', 'industry', 'basic industry', 'symbol'],  # Define the hierarchy
-    values=None,  # We are not using a numeric value, just the hierarchy
-    title="Hierarchical Visualization: Macro Sector -> Sector -> Industry -> Basic Industry",
-    color='macro sector',  # Optional: Color by macro sector
+    path=['sector', 'industry', 'basic industry', 'symbol'],
+    values=None,
+    title="Hierarchical Visualization: Sector -> Industry -> Basic Industry",
 )
 
 # Show the plot
